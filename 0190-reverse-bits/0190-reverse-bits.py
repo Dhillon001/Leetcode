2c1619bb8c728cbc96be4e35834018d18fe07aec
Harsharandeep Dhillon <139924910+Dhillon001@users.noreply.github.com>
2026-03-05 22:05:11 -0800

    Time: 49 ms (38.51%), Space: 19.1 MB (91.56%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        result = 0
+        
+        for _ in range(32):
+            bit = n & 1          # get last bit
+            result = (result << 1) | bit
+            n >>= 1              # shift n right
+            
+        return result
